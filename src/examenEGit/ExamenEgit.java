1725f3bf79927a5a2917caca4732e1aeac8d9e11
5uh4 <a_suhail.matrouch.mohamed@iespablopicasso.es>
2021-12-02 14:37:56 +0100

    Calculadora con sumas y restas. Archivo gitignore.

---
+++ src/examenEGit/ExamenEgit.java
@@ -0,0 +1,28 @@
+package examenEGit;
+
+import java.util.Scanner;
+
+public class ExamenEgit {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		System.out.println("Dame un valor");
+		int valor1 = s.nextInt();
+		System.out.println("Dame el segundo valor");
+		int valor2 = s.nextInt();
+		System.out.println("Elige el calculo a realizar: ");
+		System.out.println("1. Suma/n2. Resta/n3. Multiplicación/n4. División.");
+		int opc = s.nextInt();
+		switch (opc) {
+		case 1: {
+			System.out.println("El resultado de la suma es: " + (valor1 + valor2));
+			break;
+		}
+		case 2: {
+			System.out.println("El resultado de la resta es: " + (valor1 - valor2));
+		}
+		}
+		s.close();
+	}
+
+}
